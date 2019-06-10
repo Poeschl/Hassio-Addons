091985a5c50736d5c752660a2115906d50943311
Markus Poeschl <Poeschl@users.noreply.github.com>
2019-06-10 11:14:56 +0100

    Fixes and ssl mount

--- ICantBelieveItsNotValetudo/run.sh
+++ ICantBelieveItsNotValetudo/run.sh
@@ -1,5 +1,5 @@
 echo Setup config
-echo '{mqtt:' > /app/config.json
+echo '{"mqtt":' > /app/config.json
 cat /data/options.json >> /app/config.json
 echo ', "webserver": { "enabled": true, "port": 3000 }}' >> /app/config.json
 #cat /app/config.json
