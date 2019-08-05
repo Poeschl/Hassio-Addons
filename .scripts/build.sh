75c8c9ea8da91ab382965ea41b968543fd7cb627
Markus Poeschl <Poeschl@users.noreply.github.com>
2019-08-05 13:33:08 +0100

    check all changes in a plugin folder

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -4,7 +4,7 @@ archs="${ARCHS}"
 
 for addon in "$@"; do
 
-  if [ -z ${TRAVIS_COMMIT_RANGE} ] || git diff --name-only ${TRAVIS_COMMIT_RANGE} | grep -v README.md | grep -q ${addon}; then
+  if [ -z ${TRAVIS_COMMIT_RANGE} ] || git diff --name-only ${TRAVIS_COMMIT_RANGE} | grep -q ${addon}; then
     if [ -z "$archs" ]; then
       archs=$(jq -r '.arch // ["armv7", "armhf", "amd64", "aarch64", "i386"] | [.[] | "--" + .] | join(" ")' ${addon}/config.json)
     fi
