bf61152a5a6e40466c584cfc63787f3d177a3d9b
Markus Poeschl <Poeschl@users.noreply.github.com>
2019-10-05 00:13:08 +0100

    Remove not used code

--- MaryTTS/run.sh
+++ MaryTTS/run.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bashio
 
-bashio::log.info 'Setup language packs'
-
-# Get specified language packs
-packs=$(cat '/data/options.json' | jq '.languagePacks')
-
 bashio::log.info 'Start MaryTTS'
 bin/marytts-server
