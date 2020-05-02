988e50ca0abc8925b5e0eea772f46224346f0727
Markus Pöschl <Poeschl@users.noreply.github.com>
2020-05-02 10:34:54 +0200

    Applied Shellcheck fixes

--- OWASP-ZAP/run.sh
+++ OWASP-ZAP/run.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bashio
 set -e
 
-export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
+JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
+export JAVA_HOME
 
 bashio::log.info 'Start OWASP ZAP web interface'
 /zap/zap-webswing.sh
