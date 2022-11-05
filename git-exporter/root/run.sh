bjeanes/Poeschl-Hassio-Addons

[git-exporter] Allow customising secret patterns

git-exporter/root/run.sh
@@ -86,6 +86,22 @@ function check_secrets {
     prohibited_patterns=$(git config --get-all secrets.patterns)
     bashio::log.info "Prohibited patterns:\n${prohibited_patterns//\\n/\\\\n}"
 
+    custom_secrets=$(bashio::config 'secrets')
+    if [ ${#custom_secrets[@]} -gt 0 ]; then
+        bashio::log.info 'Add custom secrets'
+        for secret in "${custom_secrets[@]}"; do
+            git secrets --add "$secret"
+        done
+    fi
+
+    custom_allowed_secrets=$(bashio::config 'secrets')
+    if [ ${#custom_allowed_secrets[@]} -gt 0 ]; then
+        bashio::log.info 'Add custom allowed secrets'
+        for allowed_secret in "${custom_allowed_secrets[@]}"; do
+            git secrets --add -a "$allowed_secret"
+        done
+    fi
+
     bashio::log.info 'Checking for secrets'
     # shellcheck disable=SC2046
     git secrets --scan $(find $local_repository -name '*.yaml' -o -name '*.yml' -o -name '*.json' -o -name '*.disabled') \

Poeschl/Hassio-Addons

git-exporter/root/run.sh
@@ -86,6 +86,22 @@ function check_secrets {
     prohibited_patterns=$(git config --get-all secrets.patterns)
     bashio::log.info "Prohibited patterns:\n${prohibited_patterns//\\n/\\\\n}"
 
+    custom_secrets=$(bashio::config 'secrets')
+    if [ ${#custom_secrets[@]} -gt 0 ]; then
+        bashio::log.info 'Add custom secrets'
+        for secret in "${custom_secrets[@]}"; do
+            git secrets --add "$secret"
+        done
+    fi
+
+    custom_allowed_secrets=$(bashio::config 'allowed_secrets')
+    if [ ${#custom_allowed_secrets[@]} -gt 0 ]; then
+        bashio::log.info 'Add custom allowed secrets'
+        for allowed_secret in "${custom_allowed_secrets[@]}"; do
+            git secrets --add -a "$allowed_secret"
+        done
+    fi
+
     bashio::log.info 'Checking for secrets'
     # shellcheck disable=SC2046
     git secrets --scan $(find $local_repository -name '*.yaml' -o -name '*.yml' -o -name '*.json' -o -name '*.disabled') \